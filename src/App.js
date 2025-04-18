d2a089b8195602d62abf1bcb72d5b95ab581a682
walid moussa <walidmoussa00@gmail.com>
2025-04-18 19:13:16 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TipCalculator />
     </div>
   );
 }
-
-export default App;
+function TipCalculator() {
+  return (
+    <div>
+      <h1>Tip Calculator</h1>
+    </div>
+  );
+}
