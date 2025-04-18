25e80c5d00b8d7b05719bb6c97c9b6930e7a76ff
walid moussa <walidmoussa00@gmail.com>
2025-04-18 20:01:35 +0200

    tip system works only thing missing is the reset button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { useState } from "react";
 
 export default function App() {
   const [bill, setBill] = useState(0);
-  const [tip1, setTip1] = useState("dissatisfied");
+  const [tip1, setTip1] = useState("0");
+  const [tip2, setTip2] = useState("0");
   function handleBill(currbill) {
     setBill(currbill);
   }
@@ -11,10 +12,20 @@ export default function App() {
     setTip1(currTip1);
   }
 
+  function handleTip2(currTip2) {
+    setTip2(currTip2);
+  }
+
   return (
     <div>
       <BillInput onBill={handleBill} bill={bill} />
-      <SelectPercentage onTip1={handleTip1} tip={tip1} />
+      <SelectPercentage onTip1={handleTip1} tip={tip1}>
+        how do you like the service
+      </SelectPercentage>
+      <SelectPercentage onTip1={handleTip2} tip={tip2}>
+        how do your friend like the service
+      </SelectPercentage>
+      <Output bill={Number(bill)} tip1={Number(tip1)} tip2={Number(tip2)} />
     </div>
   );
 }
@@ -32,17 +43,29 @@ function BillInput({ onBill, bill }) {
     </div>
   );
 }
-function SelectPercentage({ onTip1, tip }) {
+function SelectPercentage({ onTip1, tip, children }) {
   return (
     <div>
       <h1>
-        How did you like the service?
+        {children}
         <select value={tip} onChange={(e) => onTip1(e.target.value)}>
-          <option value="dissatisfied">Dissatisfied (0%)</option>
-          <option value="notBad">Meehhh(5%)</option>
-          <option value="good">It was good(10%)</option>
-          <option value="amazing">Amazing(20%)</option>
+          <option value="0">Dissatisfied (0%)</option>
+          <option value="5">Meehhh(5%)</option>
+          <option value="10">It was good(10%)</option>
+          <option value="20">Amazing(20%)</option>
         </select>
+      </h1>
+    </div>
+  );
+}
+
+function Output({ bill, tip1, tip2 }) {
+  let tip = ((tip1 + tip2) / 2 / 100) * bill;
+  let FinalCheck = bill + tip;
+  return (
+    <div>
+      <h1>
+        You pay ${FinalCheck} (${bill} + ${tip} tip )
       </h1>
     </div>
   );
